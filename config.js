c96356e51c141f6d998b3847f78e07b3218dab67
Vu Khac Duy Kha <121989754+DuyKha09@users.noreply.github.com>
2023-02-18 18:30:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Nhấn vào';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Điều bất ngờ 18/02';
+const titleCard = '🎂Happy Birth Day🎂';
+const contentCard = 'Chúc dì Sương 1 tuổi mới vẫn luôn trẻ đẹp, luôn thành công và cưới được chồng giàu =))';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Suong.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
